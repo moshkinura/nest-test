e1eb969c7e35a04cda818fe2fba603f17b349849
Yury Moshkin <bar.u.xoloda@gmail.com>
2022-04-07 20:08:58 +0700

    stable route users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+import { UsersController } from './users/users.controller';
+// import { AppService } from './app.service';
+import { UsersService } from './users/users.service'
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+  ],
+  controllers: [
+    // AppController,
+    UsersController,
+  ],
+  providers: [
+    // AppService,
+    UsersService,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { AppModule } from './app.module';
+import { UsersModule } from './users/users.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(UsersModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('User example')
+    .setDescription('The user API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build()
+
+  const documentUsers = SwaggerModule.createDocument(app, config, {
+    include: [UsersModule]
+  })
+
+  SwaggerModule.setup('api', app, documentUsers)
+
+  await app.listen(3000)
 }
-bootstrap();
+
+bootstrap()
