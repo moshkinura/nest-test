e7bcf8b25d58ce2f1e99cc5defaa243605a74206
Yury Moshkin <bar.u.xoloda@gmail.com>
2022-04-07 20:31:11 +0700

    stable users DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,35 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: 'The ID of a user',
+    default: 1,
+    required: true,
+  })
+  id: number;
+
+  @ApiProperty({
+    description: 'The login of a user',
+    required: false,
+  })
+  login: string;
+
+  @ApiProperty({
+    description: 'The firstname of a user',
+    required: false,
+  })
+  firstname: string;
+
+  @ApiProperty({
+    description: 'The lastname of a user',
+    required: false,
+  })
+  lastname: string;
+
+  @ApiProperty({
+    description: 'The age of a user',
+    minimum: 1,
+    required: false,
+  })
+  age: number;
+}
