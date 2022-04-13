3b96b02bff21a71e3157de4bf83a9411db8f097a
Yury Moshkin <bar.u.xoloda@gmail.com>
2022-04-14 03:57:06 +0700

    add bearer auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot(config),
     // UsersModule,
-    // DevicesModule,
+    DevicesModule,
     AuthModule,
   ],
   // controllers: [
--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .setTitle('SmartHome IOT Alice Dialogs')
     .setDescription('The API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build()
 
   const document = SwaggerModule.createDocument(app, config)
